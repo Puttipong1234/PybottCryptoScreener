e591a2fbe17801ee64923c1210167b5961d2fd84
Puttipong1234 <46212403+Puttipong1234@users.noreply.github.com>
2023-05-27 16:13:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from bot import Checkport
 from config_dev import KEY, SC , LINE
 
 if __name__ == "__main__":
-    Checkport(API_KEY="EqsE39Rp781lEDZlRK3bbykarD8m3aiQjK6QbPuWESQOSywVzRQZxhkdIn6pLrU7",
-                API_SECRET="CYVGD1uvTeqe6yhndrsxQwMXR2VhXcr9mt1pv2lHnDwe3FbKNxyebRwS11tlbeV4",
-                LINE_API="Tfp2sSqZ3FVygbTFujPNG6TPFDj2YCYM35EArjrrlWF",
+    Checkport(API_KEY="xxx",
+                API_SECRET="xxx",
+                LINE_API="xxx",
                 NAME=".....")
     
     # Checkport(API_KEY="MasANgIkBzUqpPoIAHgWhQXleLfo4ZhzHlW0GKcRA1JlJtfYQPiFOhU1iFDGlMTD",
