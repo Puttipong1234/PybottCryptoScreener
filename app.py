69aace6ffe5b1878f2ea8dc419d475feee9c48e3
Puttipong1234 <46212403+Puttipong1234@users.noreply.github.com>
2023-07-15 15:10:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,4 @@ if __name__ == "__main__":
                 API_SECRET="xxx",
                 LINE_API="xxx",
                 NAME=".....")
-    
-    # Checkport(API_KEY="MasANgIkBzUqpPoIAHgWhQXleLfo4ZhzHlW0GKcRA1JlJtfYQPiFOhU1iFDGlMTD",
-    #             API_SECRET="NeriSIwzyjmsiVC4yWdZGbaMocbl076imdVKwKuGUmvpwIgvvg2l3oKxJaPrt0Vz",
-    #             LINE_API="Tfp2sSqZ3FVygbTFujPNG6TPFDj2YCYM35EArjrrlWF",
-    #             NAME="แน้ว")
-    
-    # Checkport(  API_KEY=KEY,
-    #             API_SECRET=SC,
-    #             LINE_API="1vrj2esfzzHZpyMWzKOKInSHX42YbtuM59zX1sppDta",
-    #             NAME="👦บุ๊ค👦"
-    #         )
+
